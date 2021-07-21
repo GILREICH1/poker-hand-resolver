befa8ffb0371ef1df20c93ba40fb40a024b5b2dd
Gil Reich <gilreich12@gmail.com>
2021-07-21 17:04:58 +0100

    feat: structure of functions

--- src/pokerhand.js
+++ src/pokerhand.js
@@ -1,13 +1,31 @@
 class PokerHand {
   constructor(cards) {
+    // cards is an array of 5 cards
     this.cards = cards.split(" ");
   }
-  // create an array from args
   compareWith(pokerHand) {
-    // compare with iterate thru array
+    const combo1 = findCombination(this.cards)
+    const combo2 = findCombination(pokerHand)
     // iterate through possible hand values in reverse order
     return Result.TIE;
   }
+}
+
+// returns the combination as a numeric value (e.g. 10.3 combinationScore.highCard)
+const findCombination = (hand = []) =>{
+  if (testForFlush(hand)) return 10;
+  return 0;
+}
+
+// returns true or false.
+const testForFlush = (hand)=>{
+  // testForStraight()
+  // testForFlush()
+  return false;
+}
+
+TestForStraight() {
+
 }
 
 const Result = {
@@ -15,6 +33,10 @@ const Result = {
   LOSS: 2,
   TIE: 3,
 };
+
+const cardsOrder = ["A", 1,2,3,4,5,6,7,8,9,"T","J", "Q", "K", "A"]
+// Map of combinations: score
+
 
 module.exports = { Result, PokerHand };
 // export default PokerHand;
