0f6e4156c53b5bce0a9328f894881208fa214bc7
Gil Reich <gilreich12@gmail.com>
2021-07-23 16:01:12 +0100

    getPairsScore tests

--- src/pokerhand.test.js
+++ src/pokerhand.test.js
@@ -7,12 +7,14 @@ const {
   getThreeOfKindScore,
   getFullHouseScore,
   getFOAKScore,
+  getPairScore,
 } = require("./handScorers");
 const {
   testForStraight,
   getFOAKCard,
   testForFlush,
   getPairsCards,
+  getTOAKCard,
 } = require("./testsForHands");
 
 describe("testForHands functions", () => {
@@ -36,11 +38,22 @@ describe("testForHands functions", () => {
     expect(testForFlush(flush)).toBe(true);
     expect(testForFlush(notFlush)).toBe(false);
   });
+
+  it("getTOAKCard", () => {
+    const TOAK = ["2", "2", "2", "3", "6"];
+    const noTOAK = ["2", "2", "3", "5", "6"];
+    expect(getTOAKCard(TOAK)).toBe("2");
+    expect(getTOAKCard(noTOAK)).toBe("");
+  });
+
   it("getPairsCards function", () => {
     const pair = ["2", "2", "3", "4", "5"];
     const notPair = ["A", "2", "3", "4", "5"];
     expect(getPairsCards(pair)).toEqual(["2"]);
     expect(getPairsCards(notPair)).toEqual([]);
+
+    const twoPair = ["2", "2", "3", "3", "4", "5"];
+    expect(getPairsCards(twoPair)).toEqual(["2", "3"]);
   });
 });
 
@@ -64,6 +77,12 @@ describe("handScorers functions", () => {
   it("correctly evaluates a Four of a Kind score", () => {
     const FOAKCard = "T";
     expect(getFOAKScore(FOAKCard)).toBe(8.1);
+  });
+  it("correctly evaluates pairs scores", () => {
+    const pairs = ["T"];
+    expect(getPairScore(pairs)).toBe(2.1);
+    const twoPair = ["9", "2"];
+    expect(getPairScore(twoPair)).toBe(3.09);
   });
 });
 
